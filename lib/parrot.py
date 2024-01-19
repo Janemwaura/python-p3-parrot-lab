5f692e37614d74c1ee7d7dc9fbe64e0a0c467d66
Janemwaura <mwaura461@gmail.com>
2024-01-19 16:39:01 +0300

    done

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,12 @@
-def parrot():
-    pass
+def parrot(message="Squawk!"):
+    print("Parrot says:", message)
+    return message
+
+
+custom_message = "Parrot says:"
+result = parrot(custom_message)
+print("Returned message:", result)
+
+
+default_result = parrot()
+print("Returned default message:", default_result)
